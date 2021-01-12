0e74e8a339de39bf535ed957a7f9ed441e47d46b
Dmitrij Semionov <semiond@amazon.com>
2021-01-12 11:20:14 -0500

    Removed not existing ssm document from metadata

--- setup.py
+++ setup.py
@@ -1,33 +1,8 @@
-import os
-import zipfile
-from setuptools import setup, find_packages
-
-SCRIPT_DIR = '/documents/util/scripts/src'
-SCRIPT_ZIP_FILE_NAME = 'digito_gameday_primitives.zip'
-ROOT_DIR = os.getcwd()
-
-# Zipping SSM automation document attachment.
-with zipfile.ZipFile(SCRIPT_ZIP_FILE_NAME, 'w', zipfile.ZIP_DEFLATED) as zipfile_handle:
-    for root, dirs, files in os.walk(ROOT_DIR + SCRIPT_DIR):
-        os.chdir(root)
-        for file in files:
-            zipfile_handle.write(file)
-    os.chdir(ROOT_DIR)
-
-# Adding data files documents, artifacts under build folder.
-data_files = []
-for root, dirs, files in os.walk("documents"):
-    data_files.append((root, [os.path.join(root, f) for f in files]))
-data_files.append(('.', [SCRIPT_ZIP_FILE_NAME]))
+from setuptools import setup
 
 setup(
-    name="AwsDigitoArtifactsGameday",
-    version="1.0",
-
-    # Use the pytest brazilpython runner. Provided by BrazilPython-Pytest.
-    test_command="brazilpython_pytest",
-
-    # include data files
-    data_files=data_files,
-    root_script_source_version='python3.7'
+    # TODO (semiond): configure build on CodeCommit:
+    #  https://issues.amazon.com/issues/Digito-1606
+    name="aws-digito-artifacts-gameday",
+    version="1.0"
 )
