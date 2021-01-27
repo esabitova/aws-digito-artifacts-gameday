d725cbb7b8674de812774ab5b5e4260d1bf41c6a
Priyesh Tiwari <priyesh@amazon.com>
2021-01-27 19:43:00 +0000

    commit 932242a74ed7d84bb30993ea7ae7bcf006209021 Author: Priyesh Tiwari <priyesh@amazon.com> Date: 2021-01-26T02:25:11.000Z
    Adding support for script placeholders in place of attachments
    
    cr https://code.amazon.com/reviews/CR-43859439
    
    commit f16612bb9f0190e187386fefb5a786b6eea646b0
    Author: Priyesh Tiwari <priyesh@amazon.com>
    Date: 2021-01-26T00:20:37.000Z
    
    Removing AzOutageAlarmName check temporarily

--- publisher/publish_documents.py
+++ publisher/publish_documents.py
@@ -1,28 +1,21 @@
 import boto3
 import getopt
 import glob
-import hashlib
 import json
 import logging
 import os
 import sys
-import zipfile
 from botocore.exceptions import ClientError
 
 SCRIPT_DIR = '/documents/util/scripts/src'
-SCRIPT_ZIP_FILE_NAME = 'digito_gameday_primitives.zip'
-SCRIPT_BUCKET_NAME_PREFIX = 'digito-gameday-primitives'
 
 logger = logging.getLogger('PublishDocuments')
-
 
 class PublishDocuments:
 
     def __init__(self, region):
         self.root_dir = os.getcwd()
         self.ssm = boto3.client('ssm', region_name=region)
-        self.s3 = boto3.resource('s3', region_name=region)
-        self.sts = boto3.client('sts', region_name=region)
         self.region = region
 
     def publish_document(self, list_document_metadata):
@@ -32,102 +25,56 @@ class PublishDocuments:
             doc_format = document_metadata['documentFormat']
             tag_value = document_metadata['tag']
 
-            s3_attachment_url = None
             document_content = self.get_document_content(document_metadata)
-            if 'attachments' in document_metadata:
-                s3_attachment_url = 'https://s3.amazonaws.com/' + self.get_bucket_name() + '/' + SCRIPT_ZIP_FILE_NAME
             if self.document_exists(doc_name):
                 if self.has_document_content_changed(doc_name, doc_format, document_content):
                     update_document_version = self.update_document(doc_name, document_content, doc_format,
-                                                                   s3_attachment_url, tag_value)
+                                                                   tag_value)
                     self.update_document_default_version(doc_name, update_document_version)
                     logger.info('Updated document %s' % doc_name)
                 else:
                     logger.info('Document content has not changed for document name, %s' % doc_name)
             else:
-                self.create_document(doc_name, document_content, doc_type, doc_format, s3_attachment_url, tag_value)
+                self.create_document(doc_name, document_content, doc_type, doc_format, tag_value)
                 logger.info('Created document %s' % doc_name)
 
     def get_document_content(self, document_metadata):
+        updated_document_content = ""
         with open(document_metadata['location'] + '/' + document_metadata['documentContentPath'], 'r') as f:
-            document_content = f.read()
-            if 'attachments' in document_metadata:
-                document_content = document_content + '\n' + self.get_files_attachment_snippet_in_document(
-                    document_metadata['documentFormat'])
-            return document_content
+            document_content_lines = f.read().splitlines()
+            for line in document_content_lines:
+                if ("SCRIPT_PLACEHOLDER" in line):
+                    script_placeholder = line.strip()
+                    line = self.replace_script_placeholder_in_document_content(line, script_placeholder)
 
-    def create_document(self, name, content, doc_type, doc_format, s3_attachment_url, tag_value):
-        if s3_attachment_url is None:
-            self.ssm.create_document(
-                Content=content,
-                Name=name,
-                DocumentType=doc_type,
-                DocumentFormat=doc_format,
-                Tags=[
-                    {
-                        'Key': 'Digito-reference-id',
-                        'Value': tag_value
-                    },
-                ]
-            )
-        else:
-            self.ssm.create_document(
-                Content=content,
-                Attachments=[
-                    {
-                        'Key': 'S3FileUrl',
-                        'Values': [
-                            s3_attachment_url
-                        ],
-                        'Name': SCRIPT_ZIP_FILE_NAME
-                    },
-                ],
-                Name=name,
-                DocumentType=doc_type,
-                DocumentFormat=doc_format,
-                Tags=[
-                    {
-                        'Key': 'Digito-reference-id',
-                        'Value': tag_value
-                    },
-                ]
-            )
+                updated_document_content += line + "\n"
 
-    def update_document(self, name, content, doc_format, s3_attachment_url, tag_value):
+        return updated_document_content
+
+    def create_document(self, name, content, doc_type, doc_format, tag_value):
+        self.ssm.create_document(
+            Content=content,
+            Name=name,
+            DocumentType=doc_type,
+            DocumentFormat=doc_format,
+            Tags=[
+                {
+                    'Key': 'Digito-reference-id',
+                    'Value': tag_value
+                },
+            ]
+        )
+
+    def update_document(self, name, content, doc_format, tag_value):
         update_document_response = {}
         try:
-            if s3_attachment_url is None:
-                update_document_response = self.ssm.update_document(
-                    Content=content,
-                    Name=name,
-                    DocumentVersion='$LATEST',
-                    DocumentFormat=doc_format
-                )
-            else:
-                update_document_response = self.ssm.update_document(
-                    Content=content,
-                    Attachments=[
-                        {
-                            'Key': 'S3FileUrl',
-                            'Values': [s3_attachment_url],
-                            'Name': SCRIPT_ZIP_FILE_NAME
-                        },
-                    ],
-                    Name=name,
-                    DocumentVersion='$LATEST',
-                    DocumentFormat=doc_format
-                )
-                self.ssm.add_tags_to_resource(
-                    ResourceType='Document',
-                    ResourceId=name,
-                    Tags=[
-                        {
-                            'Key': 'Digito-reference-id',
-                            'Value': tag_value
-                        },
-                    ]
-                )
-                return update_document_response['DocumentDescription']['DocumentVersion']
+            update_document_response = self.ssm.update_document(
+                Content=content,
+                Name=name,
+                DocumentVersion='$LATEST',
+                DocumentFormat=doc_format
+            )
+            return update_document_response['DocumentDescription']['DocumentVersion']
         except ClientError as e:
             logger.error('Failed to update [{}] document.'.format(name))
             raise e
@@ -162,13 +109,6 @@ class PublishDocuments:
                 return document_metadata
         except IOError as e:
             raise Exception('Could not open file %s', file_path, e)
-
-    def file_exists(self, file_path):
-        try:
-            with open(file_path):
-                return True
-        except IOError:
-            return False
 
     def get_list_of_documents(self, file_name):
         list_document_metadata = []
@@ -206,70 +146,44 @@ class PublishDocuments:
 
         return list_document_metadata
 
-    def zip_scripts(self, script_relative_path):
-        with zipfile.ZipFile(SCRIPT_ZIP_FILE_NAME, 'w', zipfile.ZIP_DEFLATED) as zipfile_handle:
-            self.zip_dir(self.root_dir + script_relative_path, zipfile_handle)
+    def replace_script_placeholder_in_document_content(self, document_content, script_placeholder):
+        script_content = self.get_script(script_placeholder)
+        document_content = document_content.replace(script_placeholder, script_content)
+        return document_content
 
-    def zip_dir(self, path, zipfile_handle):
-        for root, dirs, files in os.walk(path):
-            os.chdir(root)
-            for file in files:
-                zipfile_handle.write(file)
+    def get_script(self, script_placeholder):
+        script_file_name = script_placeholder.split("::")[1].split(".")[0]
+        script_method_name = script_placeholder.split("::")[1].split(".")[1]
 
-        os.chdir(self.root_dir)
+        script_lines = []
+        script_lines_to_be_included = ""
+        with open(self.root_dir + '/documents/util/scripts/src/' + script_file_name + '.py') as f:
+            script_lines = f.read().splitlines()
 
-    def upload_scripts_to_s3(self):
-        self.create_bucket_if_does_not_exist()
-        self.zip_scripts(SCRIPT_DIR)
-        self.upload_to_s3()
+        is_first = True
+        before_first_method = True
+        found_method = False
 
-    def upload_to_s3(self):
-        file_path = self.root_dir + '/' + SCRIPT_ZIP_FILE_NAME
-        self.s3.meta.client.upload_file(file_path, self.get_bucket_name(), SCRIPT_ZIP_FILE_NAME)
+        for line in script_lines:
+            if (before_first_method and "def " in line):
+                before_first_method = False
 
-    def create_bucket_if_does_not_exist(self):
-        try:
-            bucket_name = self.get_bucket_name()
-            self.s3.meta.client.head_bucket(Bucket=bucket_name)
-            logger.debug('Bucket %s Exists' % bucket_name)
-        except ClientError:
-            # The bucket does not exist or you have no access.
-            logger.info('Bucket %s does not exist, creating' % bucket_name)
-            # For us-east-1, s3 does not allow specifying location constraint
-            if self.region == 'us-east-1':
-                self.s3.create_bucket(Bucket=self.get_bucket_name())
-            else:
-                self.s3.create_bucket(
-                    Bucket=self.get_bucket_name(),
-                    CreateBucketConfiguration={'LocationConstraint': self.region})
+            # method ended, found another method, return
+            if (found_method and ("def " in line)):
+                return script_lines_to_be_included
 
-    def get_bucket_name(self):
-        # TODO https://issues.amazon.com/issues/Digito-671 support for custom bucket
-        return '-'.join([SCRIPT_BUCKET_NAME_PREFIX, self.get_account_id(), self.region])
+            if ("def " + script_method_name + "(" in line):
+                found_method = True
 
-    def get_account_id(self):
-        return self.sts.get_caller_identity()["Account"]
+            if ((before_first_method and script_method_name == "imports") or found_method):
+                # Add 8 spaces to all lines except first line so that it aligns with ssm document yaml format
+                if (is_first):
+                    is_first = False
+                else:
+                    line = "        " + line
+                script_lines_to_be_included += line + "\n"
 
-    def generate_file_checksum(self):
-        with open(self.root_dir + '/' + SCRIPT_ZIP_FILE_NAME, "rb") as f:
-            # read entire file as bytes
-            readable_hash = hashlib.sha256(f.read()).hexdigest()
-            return readable_hash
-
-    def get_file_size(self):
-        return os.path.getsize(self.root_dir + '/' + SCRIPT_ZIP_FILE_NAME)
-
-    def get_files_attachment_snippet_in_document(self, doc_format):
-        if doc_format == 'YAML':
-            with open(self.root_dir + '/publisher/FilesAttachmentSnippetYaml.yml', "r") as f:
-                # read file
-                snippet = f.read()
-                snippet = snippet.replace('SHA256_PLACEHOLDER', self.generate_file_checksum())
-                snippet = snippet.replace('SIZE_PLACEHOLDER', str(self.get_file_size()))
-                return snippet
-        else:
-            logger.error('Unsupported format ' % doc_format)
-
+        return script_lines_to_be_included
 
 def main(argv):
     region = 'us-west-2'
@@ -304,9 +218,6 @@ def main(argv):
 
     # get list of documents from manifest file and their required documents
     list_document_metadata = p.get_list_of_documents(file_name)
-
-    # upload python scripts to s3
-    p.upload_scripts_to_s3()
 
     # publish documents to account
     p.publish_document(list_document_metadata)
