1cfa14985c474ebedbfdeef5eb6602f0dbef1723
Dmitriy Lifanov <dlifanov@cloudgeometry.io>
2021-03-09 15:14:01 +0300

    Update docdb:sop:scale_down:2020-09-21 SSM

--- documents/util/scripts/src/docdb_util.py
+++ documents/util/scripts/src/docdb_util.py
@@ -23,3 +23,50 @@ def create_new_instance(events, context):
         return {'instance_az':instance_az}
     except Exception as e:
         print(f'Error: {e}')
+
+def count_cluster_instances(events, context):
+
+    try:
+        print(events['DbClusterInstances'])
+        return {'DbClusterInstancesNumber' : len(events['DbClusterInstances'])}
+    except Exception as e:
+        print(f'Error: {e}')
+
+def verify_db_instance_exist(events, context):
+    try:
+        docdb = boto3.client('docdb')
+        response = docdb.describe_db_instances(
+            DBInstanceIdentifier=events['DBInstanceIdentifier'],
+            Filters=[
+                {
+                    'Name': 'db-cluster-id',
+                    'Values': [
+                        events['DBClusterIdentifier'],
+                    ]
+                },
+            ]
+        )
+        return {events['DBInstanceIdentifier'] : response['DBInstances'][0]['DBInstanceStatus']}
+
+    except Exception as e:
+        if "DBInstanceNotFound" in str(e):
+            raise Exception(f"Cluster instance {events['DBInstanceIdentifier']} is not found in cluster {events['DBClusterIdentifier']}")
+        else:
+            print(f'Error: {e}')
+            raise
+
+def verify_cluster_instances(events, context):
+    try:
+        docdb = boto3.client('docdb')
+
+        while True:
+            response = docdb.describe_db_clusters(DBClusterIdentifier=events['DBClusterIdentifier'])
+            current_instances_number = len(response['DBClusters'][0]['DBClusterMembers'])
+            before_instances_number = events['BeforeDbClusterInstancesNumber']
+            time.sleep(5)
+            if current_instances_number < before_instances_number:
+                return {'DbClusterInstancesNumber' : current_instances_number}
+                break
+
+    except Exception as e:
+        print(f'Error: {e}')
