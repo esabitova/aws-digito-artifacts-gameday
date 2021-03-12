899e2322cd3e3574fd21b031b65e02061718e4a0
Bulat Usmanov <busmanov@upteam.com>
2021-03-12 11:49:03 +0300

    DIG-770 add test_linter unit_test to common.mk

--- common.mk
+++ common.mk
@@ -1,4 +1,5 @@
 SHELL := /bin/bash
+CWD:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 
 
 clean:
@@ -11,3 +12,23 @@ pip_install:
 	source venv/bin/activate && \
 	pip3 install -r requirements.txt && \
 	deactivate
+
+# Execute linter to test code style
+test_linter:
+# If it was executed from the nested Makefiles when workdir was not changed after moving to the parent Makefile
+	cd "$(CWD)/" && \
+	source venv/bin/activate && \
+	python3 -m flake8 --config setup.cfg && \
+	deactivate
+
+
+# Execute unit tests
+unit_test:
+	# If it was executed from the nested Makefiles when workdir was not changed after moving to the parent Makefile
+	cd "$(CWD)/" && \
+	source venv/bin/activate && \
+	python3 -m pytest -m unit_test && \
+	deactivate
+
+# Wrapper rule to execute unit tests and linter together easily in the nested Makefiles
+linter_and_unit_test: test_linter unit_test
