875cb2501a173f780ff789e4a1fcfb45439c5867
Priyesh Tiwari <priyesh@amazon.com>
2021-03-12 12:26:19 -0800

    fixing flake8 errors
    cr https://code.amazon.com/reviews/CR-47045871

--- documents/rds/test/instance_reboot/2020-04-01/Tests/step_defs/test_instance_reboot.py
+++ documents/rds/test/instance_reboot/2020-04-01/Tests/step_defs/test_instance_reboot.py
@@ -4,8 +4,8 @@ from pytest_bdd import (
     scenario
 )
 
+
 @scenario('../../Tests/features/instance_reboot.feature', 'Create AWS resources using CloudFormation template '
-                                                                  'and execute SSM automation document to reboot '
-                                                                  'RDS instance')
+          'and execute SSM automation document to reboot RDS instance')
 def test_force_maz_failover():
     """Create AWS resources using CloudFormation template and execute SSM automation document."""
