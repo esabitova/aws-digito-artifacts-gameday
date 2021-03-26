bc6e6842962da70c23fd654197f57a2061c34e42
iderbenev <iderbenev@upteam.com>
2021-03-26 18:44:32 +0300

    DIG-507 [docdb] add wait steps for docdb reboot test

--- documents/docdb/conftest.py
+++ documents/docdb/conftest.py
@@ -1,10 +1,24 @@
 from pytest_bdd import (
     given,
-    parsers, when
+    parsers, when, then
 )
+
+import time
 
 from resource_manager.src.util import docdb_utils as docdb_utils
 from resource_manager.src.util.common_test_utils import extract_param_value, put_to_ssm_test_cache
+from resource_manager.src.util.param_utils import parse_param_values_from_table
+
+
+def docdb_instance_status(db_instance_identifier, boto3_session):
+    try:
+        docdb = boto3_session.client('docdb')
+        response = docdb.describe_db_instances(DBInstanceIdentifier=db_instance_identifier)
+        current_instance_status = response['DBInstances'][0]['DBInstanceStatus']
+        return {'DBInstanceStatus': current_instance_status}
+    except Exception as e:
+        print(f'Error: {e}')
+        raise
 
 
 cache_number_of_clusters_expression = 'cache current number of clusters as "{cache_property}" "{step_key}" SSM ' \
@@ -22,3 +36,35 @@ def cache_number_of_instances(
     )
     number_of_instances = docdb_utils.get_number_of_instances(boto3_session, cluster_id)
     put_to_ssm_test_cache(ssm_test_cache, step_key, cache_property, number_of_instances)
+
+
+@when(parsers.parse('Wait for DocumentDB instance is in "{expected_status}" status for '
+                    '"{time_to_wait}" seconds\n{input_parameters}'))
+@then(parsers.parse('Wait for DocumentDB instance is in "{expected_status}" status for '
+                    '"{time_to_wait}" seconds\n{input_parameters}'))
+def wait_for_documentdb_with_params(cfn_output_params, time_to_wait, boto3_session,
+                                    expected_status, input_parameters, ssm_test_cache):
+    """
+    Common step to wait for SSM document execution step waiting of final status
+    :param cfn_output_params The cfn output params from resource manager
+    :param boto3_session boto3 client session
+    :param time_to_wait Timeout in seconds to wait until step status is resolved
+    :param expected_status The expected SSM document execution status
+    :param input_parameters The input parameters
+    :param ssm_test_cache The custom test cache
+    """
+    parameters = parse_param_values_from_table(input_parameters, {'cache': ssm_test_cache,
+                                                                  'cfn-output': cfn_output_params})
+    db_instance_identifier = parameters[0].get('DBInstanceIdentifier')
+    if db_instance_identifier is None:
+        raise Exception('Parameter with name [DBInstanceIdentifier] should be provided')
+
+    actual_status = 'Unidentified'
+    timeout_timestamp = time.time() + int(time_to_wait)
+    wait_condition = True
+    while wait_condition:
+        actual_status = docdb_instance_status(db_instance_identifier, boto3_session)['DBInstanceStatus']
+        if actual_status == expected_status or time.time() > timeout_timestamp:
+            wait_condition = False
+        time.sleep(5)
+    assert actual_status == expected_status
--- documents/docdb/sop/reboot_db_instance/2020-09-21/Tests/features/reboot_db_instance.feature
+++ documents/docdb/sop/reboot_db_instance/2020-09-21/Tests/features/reboot_db_instance.feature
@@ -8,9 +8,14 @@ Feature: SSM automation document to recover the database into a known good state
       | documents/docdb/sop/reboot_db_instance/2020-09-21/Documents/AutomationAssumeRoleTemplate.yml | ASSUME_ROLE  |
     And published "Digito-RebootDbInstance_2020-09-21" SSM document
     When SSM automation document "Digito-RebootDbInstance_2020-09-21" executed
-      | DBClusterIdentifier                              | DBInstanceIdentifier                                      | AutomationAssumeRole                                                         |
+      | DBClusterIdentifier                              | DBInstanceIdentifier                                     | AutomationAssumeRole                                                         |
       | {{cfn-output:DocDbTemplate>DBClusterIdentifier}} | {{cfn-output:DocDbTemplate>DBInstanceReplicaIdentifier}} | {{cfn-output:AutomationAssumeRoleTemplate>DigitoRebootDbInstanceAssumeRole}} |
-
+    And Wait for DocumentDB instance is in "rebooting" status for "100" seconds
+      | DBInstanceIdentifier                                     |
+      | {{cfn-output:DocDbTemplate>DBInstanceReplicaIdentifier}} |
     Then SSM automation document "Digito-RebootDbInstance_2020-09-21" execution in status "Success"
       | ExecutionId                |
       | {{cache:SsmExecutionId>1}} |
+    And Wait for DocumentDB instance is in "available" status for "100" seconds
+      | DBInstanceIdentifier                                     |
+      | {{cfn-output:DocDbTemplate>DBInstanceReplicaIdentifier}} |
