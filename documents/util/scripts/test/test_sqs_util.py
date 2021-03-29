bef34d84bdc7b84920d6e51b9abb001a0e33ae02
Bulat Usmanov <busmanov@upteam.com>
2021-03-29 18:52:19 +0300

    DIG-277 some unit tests

--- documents/util/scripts/test/test_sqs_util.py
+++ documents/util/scripts/test/test_sqs_util.py
@@ -5,16 +5,19 @@ from unittest.mock import patch, MagicMock
 
 import pytest
 
-from documents.util.scripts.src.sqs_util import add_deny_in_sqs_policy, revert_sqs_policy
+from documents.util.scripts.src.sqs_util import add_deny_in_sqs_policy, revert_sqs_policy, transfer_messages
 from documents.util.scripts.src.sqs_util import send_message_of_size
 
 SQS_STANDARD_QUEUE_URL = "https://sqs.us-east-2.amazonaws.com/123456789012/MyQueue"
+SQS_STANDARD_DEST_QUEUE_URL = "https://sqs.us-east-2.amazonaws.com/123456789012/MyDestQueue"
 SQS_FIFO_QUEUE_URL = "https://sqs.us-east-2.amazonaws.com/123456789012/MyQueue.fifo"
 
 
 @pytest.mark.unit_test
 class TestSqsUtil(unittest.TestCase):
     def setUp(self):
+        self.number_of_messages_to_transfer = 40
+        self.messages_transfer_batch_size = 10
         self.queue_url = SQS_STANDARD_QUEUE_URL
         self.empty_policy = {"Policy": ""}
         self.resource = "arn:aws:sqs:us-east-2:444455556666:queue1"
@@ -128,3 +131,76 @@ class TestSqsUtil(unittest.TestCase):
         send_message_of_size(events, None)
         self.client.send_message.assert_called_once()
         self.assertEqual(100, len(self.client.send_message.call_args[1]['MessageBody']))
+
+    def test_transfer_messages_empty_events(self):
+        events = {}
+        self.assertRaises(KeyError, transfer_messages, events, None)
+        self.client.receive_message.assert_not_called()
+
+    def test_transfer_messages_empty_sqs_standard_queue_url(self):
+        events = {
+            "TargetQueueUrl": SQS_STANDARD_QUEUE_URL,
+            "NumberOfMessagesToTransfer": 123,
+            "MessagesTransferBatchSize": 123,
+            "ForceExecution": True
+        }
+        self.assertRaises(KeyError, transfer_messages, events, None)
+        self.client.receive_message.assert_not_called()
+
+    def test_transfer_messages_empty_target_queue_url(self):
+        events = {
+            "SourceQueueUrl": SQS_STANDARD_QUEUE_URL,
+            "NumberOfMessagesToTransfer": 123,
+            "MessagesTransferBatchSize": 123,
+            "ForceExecution": True
+        }
+        self.assertRaises(KeyError, transfer_messages, events, None)
+        self.client.receive_message.assert_not_called()
+
+    def test_transfer_messages_empty_number_of_messages_to_transfer(self):
+        events = {
+            "SourceQueueUrl": SQS_STANDARD_QUEUE_URL,
+            "TargetQueueUrl": SQS_STANDARD_QUEUE_URL,
+            "MessagesTransferBatchSize": 123,
+            "ForceExecution": True
+        }
+        self.assertRaises(KeyError, transfer_messages, events, None)
+        self.client.receive_message.assert_not_called()
+
+    def test_transfer_messages_empty_messages_transfer_batch_size(self):
+        events = {
+            "SourceQueueUrl": SQS_STANDARD_QUEUE_URL,
+            "TargetQueueUrl": SQS_STANDARD_QUEUE_URL,
+            "NumberOfMessagesToTransfer": 123,
+            "ForceExecution": True
+        }
+        self.assertRaises(KeyError, transfer_messages, events, None)
+        self.client.receive_message.assert_not_called()
+
+    def test_transfer_messages_empty_force_execution(self):
+        events = {
+            "SourceQueueUrl": SQS_STANDARD_QUEUE_URL,
+            "TargetQueueUrl": SQS_STANDARD_QUEUE_URL,
+            "MessagesTransferBatchSize": 123,
+            "ForceExecution": True
+        }
+        self.assertRaises(KeyError, transfer_messages, events, None)
+        self.client.receive_message.assert_not_called()
+
+    # def test_transfer_messages(self):
+    #     events = {
+    #         "SourceQueueUrl": SQS_STANDARD_QUEUE_URL,
+    #         "TargetQueueUrl": SQS_STANDARD_DEST_QUEUE_URL,
+    #         "MessagesTransferBatchSize": self.messages_transfer_batch_size,
+    #         "NumberOfMessagesToTransfer": self.number_of_messages_to_transfer,
+    #         "ForceExecution": True
+    #     }
+    #     transfer_messages(events, None)
+    #     self.client.get_queue_attributes.assert_called_once_with(QueueUrl=SQS_STANDARD_QUEUE_URL,
+    #                                                              AttributeNames=['FifoQueue'])
+    #     self.client.get_queue_attributes.assert_called_once_with(QueueUrl=SQS_STANDARD_DEST_QUEUE_URL,
+    #                                                              AttributeNames=['FifoQueue'])
+    #     self.client.receive_message.assert_called_with(QueueUrl=SQS_STANDARD_QUEUE_URL,
+    #                                                    MaxNumberOfMessages=self.messages_transfer_batch_size,
+    #                                                    MessageAttributeNames=['All'],
+    #                                                    AttributeNames=['All'])
