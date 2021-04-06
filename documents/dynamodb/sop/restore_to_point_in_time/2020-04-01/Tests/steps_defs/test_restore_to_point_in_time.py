396de2c613556ca24443630f2d7deec4407887f6
Vladimir Semashkin <vsemashkin@upteam.com>
2021-04-07 00:17:00 +0400

    dynamodb:sop:restore:added test for stream

--- documents/dynamodb/sop/restore_to_point_in_time/2020-04-01/Tests/steps_defs/test_restore_to_point_in_time.py
+++ documents/dynamodb/sop/restore_to_point_in_time/2020-04-01/Tests/steps_defs/test_restore_to_point_in_time.py
@@ -12,6 +12,13 @@ def test_restore_to_latest_point_in_time():
     """Create AWS resources using CloudFormation template and execute SSM automation document."""
 
 
+@scenario('../features/restore_to_latest_point_in_time.feature',
+          'Restores table to the latest available point. '
+          'In the source table the stream is enabled')
+def test_restore_to_latest_point_in_time_stream_enabled():
+    """Create AWS resources using CloudFormation template and execute SSM automation document."""
+
+
 @scenario('../features/restore_to_specific_point_in_time.feature',
           'Restores table to the specific point')
 def test_restore_to_specific_point_in_time():
