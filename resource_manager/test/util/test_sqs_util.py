8d61d0136c9af820486623a4f26c02f53fb69781
semiond <semiond@amazon.com>
2021-04-06 19:03:17 +0000

    Squashed commit of the following
    commit b2204b3f 
    Author: Bulat Usmanov <busmanov@upteam.com> 
    Date: Tue Apr 06 2021 14:57:19 GMT-0400 (Eastern Daylight Time) 
    
        DIG-873 Increase code coverage over 83% 2
    
    
    commit cd655a11 
    Author: Bulat Usmanov <busmanov@upteam.com> 
    Date: Tue Apr 06 2021 14:45:39 GMT-0400 (Eastern Daylight Time) 
    
        DIG-873 Increase code coverage over 83%

--- resource_manager/test/util/test_sqs_util.py
+++ resource_manager/test/util/test_sqs_util.py
@@ -1,7 +1,9 @@
 import unittest
-import pytest
-from unittest.mock import MagicMock
 from datetime import datetime
+from unittest.mock import MagicMock
+
+import pytest
+
 import resource_manager.src.util.sqs_utils as sqs_utils
 
 SQS_QUEUE_URL = "https://this.is.some.url"
@@ -9,6 +11,11 @@ SQS_FIFO_QUEUE_URL = "https://this.is.some.url.fifo"
 SQS_MESSAGE_BODY = "some message body"
 SQS_FIFO_MESSAGE_GROUP = "message-group-id"
 SQS_POLICY = '{"some-field": "some-value", "some-other-field": "some-other-value"}'
+ATTRIBUTES = {
+    'Attributes': {
+        'ApproximateNumberOfMessages': "10",
+        'Policy': SQS_POLICY}
+}
 
 
 def get_queue_attributes_side_effect(number_of_messages):
@@ -38,6 +45,12 @@ class TestSQSUtil(unittest.TestCase):
     def test_send_message_to_queue(self):
         sqs_utils.send_message_to_standard_queue(self.session_mock, SQS_QUEUE_URL, SQS_MESSAGE_BODY)
         self.mock_sqs_service.send_message.assert_called_once_with(QueueUrl=SQS_QUEUE_URL, MessageBody=SQS_MESSAGE_BODY)
+
+    def test_send_message_to_queue_with_attributes(self):
+        sqs_utils.send_message_to_standard_queue(self.session_mock, SQS_QUEUE_URL, SQS_MESSAGE_BODY, ATTRIBUTES)
+        self.mock_sqs_service.send_message.assert_called_once_with(QueueUrl=SQS_QUEUE_URL,
+                                                                   MessageBody=SQS_MESSAGE_BODY,
+                                                                   MessageAttributes=ATTRIBUTES)
 
     def test_send_message_to_fifo_queue(self):
         now = datetime.now().isoformat()
