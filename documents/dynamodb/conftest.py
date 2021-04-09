c2e024df22bb433599969d79e5b4d2e1ab7ee02a
Evgeniy Dmitriev <edmitriev@upteam.com>
2021-04-09 20:16:19 +0700

    DIG-524 Updated conftest.py

--- documents/dynamodb/conftest.py
+++ documents/dynamodb/conftest.py
@@ -28,8 +28,8 @@ def cache_table_property(resource_manager, ssm_test_cache, boto3_session, json_p
 @when(parse('generate different value of "{target_property}" than "{old_property}" from "{from_range}" to'
             ' "{to_range}" as "{new_property}" "{step_key}" SSM automation execution'
             '\n{input_parameters}'))
-def generate_different_value(resource_manager, ssm_test_cache, target_property, old_property, from_range, to_range,
-                             new_property, step_key, input_parameters):
+def generate_and_cache_different_value_by_property_name(resource_manager, ssm_test_cache, old_property, from_range,
+                                                        to_range, new_property, step_key, input_parameters):
     old_value = extract_param_value(input_parameters, old_property, resource_manager, ssm_test_cache)
     new_value = generate_different_value_by_ranges(int(from_range), int(to_range), int(old_value))
     put_to_ssm_test_cache(ssm_test_cache, step_key, new_property, new_value)
