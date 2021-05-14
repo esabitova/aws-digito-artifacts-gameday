6df4a3cf4a0f2764cc070199279f2296da3b23bf
Vladimir Semashkin <vsemashkin@upteam.com>
2021-05-14 21:47:30 +0400

    merge

--- documents/dynamodb/conftest.py
+++ documents/dynamodb/conftest.py
@@ -26,12 +26,12 @@ def cache_table_property(resource_pool, ssm_test_cache, boto3_session, json_path
 @given(parsers.parse("wait table {table_name_ref} to be active "
                      "for {wait_sec} seconds with interval {delay_sec} seconds"))
 def wait_table_to_be_active_for_x_seconds(ssm_test_cache,
-                                          resource_manager,
+                                          resource_pool,
                                           boto3_session,
                                           table_name_ref,
                                           wait_sec,
                                           delay_sec):
-    cf_output = resource_manager.get_cfn_output_params()
+    cf_output = resource_pool.get_cfn_output_params()
     table_name = param_utils.parse_param_value(table_name_ref, {'cfn-output': cf_output, 'cache': ssm_test_cache})
     wait_table_to_be_active(table_name=table_name,
                             boto3_session=boto3_session,
@@ -44,12 +44,12 @@ def wait_table_to_be_active_for_x_seconds(ssm_test_cache,
 @then(parsers.parse("drop Dynamo DB table with the name {table_name_ref} and wait "
                     "for {wait_sec} seconds with interval {delay_sec} seconds"))
 def drop_and_wait_dynamo_db_table(ssm_test_cache,
-                                  resource_manager,
+                                  resource_pool,
                                   boto3_session,
                                   table_name_ref,
                                   wait_sec,
                                   delay_sec):
-    cf_output = resource_manager.get_cfn_output_params()
+    cf_output = resource_pool.get_cfn_output_params()
     table_name = param_utils.parse_param_value(table_name_ref, {'cfn-output': cf_output, 'cache': ssm_test_cache})
     drop_and_wait_dynamo_db_table_if_exists(table_name=table_name,
                                             boto3_session=boto3_session,
@@ -59,11 +59,11 @@ def drop_and_wait_dynamo_db_table(ssm_test_cache,
 
 @given(parsers.parse("find a valid recovery point in time for {table_name_ref} and cache it as {field_name}"))
 def find_valid_recovery_point_in_time(ssm_test_cache,
-                                      resource_manager,
+                                      resource_pool,
                                       boto3_session,
                                       table_name_ref,
                                       field_name):
-    cf_output = resource_manager.get_cfn_output_params()
+    cf_output = resource_pool.get_cfn_output_params()
     table_name = param_utils.parse_param_value(table_name_ref, {'cfn-output': cf_output, 'cache': ssm_test_cache})
     valid_recovery_point = \
         get_earliest_recovery_point_in_time(table_name=table_name, boto3_session=boto3_session)
@@ -75,13 +75,13 @@ def find_valid_recovery_point_in_time(ssm_test_cache,
                      "{region_global_table_ref} and wait for "
                      "{wait_sec} seconds with delay {delay_sec} seconds"))
 def enable_global_table(ssm_test_cache,
-                        resource_manager,
+                        resource_pool,
                         boto3_session,
                         table_name_ref,
                         region_global_table_ref,
                         wait_sec,
                         delay_sec):
-    cf_output = resource_manager.get_cfn_output_params()
+    cf_output = resource_pool.get_cfn_output_params()
     table_name = param_utils.parse_param_value(table_name_ref, {'cfn-output': cf_output, 'cache': ssm_test_cache})
     region_global_table = param_utils.parse_param_value(
         region_global_table_ref, {'cfn-output': cf_output, 'cache': ssm_test_cache})
@@ -96,13 +96,13 @@ def enable_global_table(ssm_test_cache,
                     "{region_global_table_ref} and wait for "
                     "{wait_sec} seconds with delay {delay_sec} seconds"))
 def disable_global_table(ssm_test_cache,
-                         resource_manager,
+                         resource_pool,
                          boto3_session,
                          table_name_ref,
                          region_global_table_ref,
                          wait_sec,
                          delay_sec):
-    cf_output = resource_manager.get_cfn_output_params()
+    cf_output = resource_pool.get_cfn_output_params()
     table_name = param_utils.parse_param_value(table_name_ref, {'cfn-output': cf_output, 'cache': ssm_test_cache})
     region_global_table = param_utils.parse_param_value(
         region_global_table_ref, {'cfn-output': cf_output, 'cache': ssm_test_cache})
