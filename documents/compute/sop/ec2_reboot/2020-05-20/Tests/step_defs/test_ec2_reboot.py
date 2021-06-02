afaf728be5957e9fa54f8235b23d03a06c49b1c1
semiond <semiond@amazon.com>
2021-06-02 13:26:03 +0000

    Squashed commit of the following
    commit 5df5c87b 
    Author: iderbenev <iderbenev@upteam.com> 
    Date: Wed Jun 02 2021 06:54:25 GMT-0400 (Eastern Daylight Time) 
    
        DIG-969 [ASG] fix cfn

--- documents/compute/sop/ec2_reboot/2020-05-20/Tests/step_defs/test_ec2_reboot.py
+++ documents/compute/sop/ec2_reboot/2020-05-20/Tests/step_defs/test_ec2_reboot.py
@@ -7,5 +7,3 @@ from pytest_bdd import scenario
           'Execute SSM automation document Digito-Ec2Reboot_2020-05-20')
 def test_ec2_reboot_usual_case():
     """Execute SSM automation document Digito-Ec2Reboot_2020-05-20"""
-
-
