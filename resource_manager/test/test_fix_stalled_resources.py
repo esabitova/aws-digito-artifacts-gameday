bcc603d1ec92b1ac1de7a339977d09ac51e270ac
Dmitrij Semionov <semiond@amazon.com>
2021-06-03 12:53:58 -0400

    Adding aws account id for S3 in fix_integ_test_stalled_resources

--- resource_manager/test/test_fix_stalled_resources.py
+++ resource_manager/test/test_fix_stalled_resources.py
@@ -11,8 +11,10 @@ class TestFixIntegTestStalledResources(unittest.TestCase):
 
     def test_fix_integ_test_stalled_resources(self):
         boto3_session = boto3.Session(profile_name='default')
+        aws_account_id = boto3_session.client('sts').get_caller_identity().get('Account')
+
         cfn_helper = CloudFormationTemplate(boto3_session)
-        s3_helper = S3(boto3_session)
+        s3_helper = S3(boto3_session, aws_account_id)
         rm = ResourcePool(cfn_helper, s3_helper, dict(), None, None)
         rm.init_ddb_tables(boto3_session)
         rm.fix_stalled_resources()
