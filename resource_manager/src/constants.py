75af1de51d5699d6519061b9472779fb20ccc103
Dmitrij Semionov <semiond@amazon.com>
2021-06-11 09:15:35 -0600

    Increased wait_time_out_secs 3600 -> 7200 seconds

--- resource_manager/src/constants.py
+++ resource_manager/src/constants.py
@@ -1,7 +1,7 @@
 # Number of seconds to wait for resources per waiting iteration
 sleep_time_secs = 5
 # Number of seconds to wait for resources before time out
-wait_time_out_secs = 3600
+wait_time_out_secs = 7200
 # Cloud formation step completion wait time per iteration
 cf_operation_sleep_time_secs = 20
 # S3 bucket name patter for integration test cfn templates
