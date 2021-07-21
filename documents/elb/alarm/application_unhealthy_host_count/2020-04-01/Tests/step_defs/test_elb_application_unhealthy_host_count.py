dfe7b9fa4fdc1826c55b224ababec59c1091bf62
Aydyn Tairov <den.tairov@gmail.com>
2021-07-21 12:49:21 +0300

    DIG-1113 add red case for elb_application_unhealthy_host_count

--- documents/elb/alarm/application_unhealthy_host_count/2020-04-01/Tests/step_defs/test_elb_application_unhealthy_host_count.py
+++ documents/elb/alarm/application_unhealthy_host_count/2020-04-01/Tests/step_defs/test_elb_application_unhealthy_host_count.py
@@ -9,3 +9,9 @@ from pytest_bdd import (
           'less than a threshold - green')
 def test_elb_application_unhealthy_host_count_alarm_green():
     pass
+
+
+@scenario('../features/elb_application_unhealthy_host_count.feature',
+          'Report when count of application load balancer unhealthy hosts greater than or equal to a threshold - red')
+def test_elb_application_unhealthy_host_count_exceeded_threshold_alarm():
+    pass
