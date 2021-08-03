152f5470843e8448c31dc7d2479080d30bee0823
Evgeniy Dmitriev <edmitriev@upteam.com>
2021-08-03 19:11:48 +0000

    DIG-235 Full Cucumber test for elasticache:sop:promote_replica_cluster_mode_disabled:2020-10-26

--- documents/util/scripts/test/test_elasticache_util.py
+++ documents/util/scripts/test/test_elasticache_util.py
@@ -1,32 +1,52 @@
 import unittest
-import pytest
-import documents.util.scripts.src.elasticache_util as elasticache_util
-import documents.util.scripts.test.test_data_provider as test_data_provider
 from unittest.mock import patch, MagicMock
 
+import pytest
+
+import documents.util.scripts.src.elasticache_util as elasticache_util
+import documents.util.scripts.test.test_data_provider as test_data_provider
 from documents.util.scripts.test.mock_sleep import MockSleep
 
-ELASTICACHE_REPLICATION_GROUP_ID = "rd-id-1234567890"
+REPLICATION_GROUP_ID = 'redis-non-cluster-single-az-7174ddd0'
+PRIMARY_CLUSTER_ID = REPLICATION_GROUP_ID + '001'
 
 
-def get_describe_replication_groups(rg_id, memberclusters_count, node_count):
-    member_clusters = [f"{rg_id}-00{x}" for x in range(1, memberclusters_count + 1)]
-    replicas = [
-        {
-            "CacheClusterId": f"{rg_id}-00{x}",
-            "CacheNodeId": "0001",
-            "ReadEndpoint": {
-                "Address": f"{rg_id}-00{x}.00yu6x.0001.euc1.cache.amazonaws.com",
-                "Port": 6379
-            },
-            "PreferredAvailabilityZone": "eu-central-1a",
-            "CurrentRole": "replica"
-        } for x in range(2, node_count + 1)]
-    nodegroup_members = [{
-        "CacheClusterId": f"{rg_id}-001",
+def get_sample_describe_replication_groups_response(**kwargs):
+    return {
+        'ReplicationGroups': [
+            {
+                'ReplicationGroupId': REPLICATION_GROUP_ID,
+                'AutomaticFailover': kwargs.get('AutomaticFailover', 'disabled'),
+                'MultiAZ': kwargs.get('MultiAZ', 'disabled'),
+                'ClusterEnabled': kwargs.get('ClusterEnabled', False),
+                'Status': kwargs.get('Status', 'available')
+            }
+        ],
+        "ResponseMetadata": {
+            "HTTPStatusCode": 200
+        }
+    }
+
+
+def get_describe_replication_groups(replication_group_id, member_clusters_count, node_count):
+    member_clusters = [f"{replication_group_id}-00{x}" for x in range(1, member_clusters_count + 1)]
+
+    replicas = [{
+        "CacheClusterId": f"{replication_group_id}-00{x}",
         "CacheNodeId": "0001",
         "ReadEndpoint": {
-            "Address": f"{rg_id}-001.00yu6x.0001.euc1.cache.amazonaws.com",
+            "Address": f"{replication_group_id}-00{x}.00yu6x.0001.euc1.cache.amazonaws.com",
+            "Port": 6379
+        },
+        "PreferredAvailabilityZone": "eu-central-1a",
+        "CurrentRole": "replica"
+    } for x in range(2, node_count + 1)]
+
+    node_group_members = [{
+        "CacheClusterId": f"{replication_group_id}-001",
+        "CacheNodeId": "0001",
+        "ReadEndpoint": {
+            "Address": f"{replication_group_id}-001.00yu6x.0001.euc1.cache.amazonaws.com",
             "Port": 6379
         },
         "PreferredAvailabilityZone": "eu-central-1a",
@@ -36,7 +56,7 @@ def get_describe_replication_groups(rg_id, memberclusters_count, node_count):
     result = {
         "ReplicationGroups": [
             {
-                "ReplicationGroupId": rg_id,
+                "ReplicationGroupId": replication_group_id,
                 "Description": "ElastiCache",
                 "GlobalReplicationGroupInfo": {},
                 "Status": "available",
@@ -47,14 +67,14 @@ def get_describe_replication_groups(rg_id, memberclusters_count, node_count):
                         "NodeGroupId": "0001",
                         "Status": "available",
                         "PrimaryEndpoint": {
-                            "Address": f"{rg_id}.00yu6x.ng.0001.euc1.cache.amazonaws.com",
+                            "Address": f"{replication_group_id}.00yu6x.ng.0001.euc1.cache.amazonaws.com",
                             "Port": 6379
                         },
                         "ReaderEndpoint": {
-                            "Address": f"{rg_id}-ro.00yu6x.ng.0001.euc1.cache.amazonaws.com",
+                            "Address": f"{replication_group_id}-ro.00yu6x.ng.0001.euc1.cache.amazonaws.com",
                             "Port": 6379
                         },
-                        "NodeGroupMembers": nodegroup_members
+                        "NodeGroupMembers": node_group_members
                     }
                 ],
                 "AutomaticFailover": "disabled",
@@ -66,7 +86,8 @@ def get_describe_replication_groups(rg_id, memberclusters_count, node_count):
                 "AuthTokenEnabled": False,
                 "TransitEncryptionEnabled": False,
                 "AtRestEncryptionEnabled": False,
-                "ARN": f"arn:aws:elasticache:eu-central-1:{test_data_provider.ACCOUNT_ID}:replicationgroup:{rg_id}"
+                "ARN": f"arn:aws:elasticache:eu-central-1:{test_data_provider.ACCOUNT_ID}:replicationgroup:"
+                       f"{replication_group_id}"
             }
         ]
     }
@@ -74,58 +95,95 @@ def get_describe_replication_groups(rg_id, memberclusters_count, node_count):
 
 
 @pytest.mark.unit_test
-class TestBackupUtil(unittest.TestCase):
+class TestElasticacheUtil(unittest.TestCase):
     def setUp(self):
         self.patcher = patch('boto3.client')
         self.client = self.patcher.start()
         self.mock_elasticache = MagicMock()
         self.side_effect_map = {
-            'elasticache': self.mock_elasticache,
+            'elasticache': self.mock_elasticache
         }
         self.client.side_effect = lambda service_name, config=None: self.side_effect_map.get(service_name)
 
     def tearDown(self):
         self.patcher.stop()
 
-    def test_check_required_params(self):
-        events = {
-            'test': 'foo'
-        }
-        required_params = [
-            'test',
-            'test2'
-        ]
-        with pytest.raises(KeyError) as key_error:
-            elasticache_util.check_required_params(required_params, events)
-        assert 'Requires test2 in events' in str(key_error.value)
+    def test_assert_cluster_mode_disabled(self):
+        events = {'ReplicationGroupId': REPLICATION_GROUP_ID}
+        self.mock_elasticache.describe_replication_groups.return_value = \
+            get_sample_describe_replication_groups_response(ClusterEnabled=False)
+        elasticache_util.assert_cluster_mode_disabled(events, None)
+        self.mock_elasticache.describe_replication_groups.assert_called_with(
+            ReplicationGroupId=REPLICATION_GROUP_ID
+        )
 
-    def test_verify_all_nodes_in_rg_available(self):
+    def test_assert_cluster_mode_disabled_invalid_status(self):
+        events = {'ReplicationGroupId': REPLICATION_GROUP_ID}
+        self.mock_elasticache.describe_replication_groups.return_value = \
+            get_sample_describe_replication_groups_response(ClusterEnabled=True)
+
+        with pytest.raises(AssertionError) as exception_info:
+            elasticache_util.assert_cluster_mode_disabled(events, None)
+        self.assertTrue(exception_info.match('.*'))
+
+    def test_check_required_params(self):
+        events = {'parameter_one': 'foo'}
+        required_params = ['parameter_one',
+                           'parameter_two']
+        with pytest.raises(KeyError) as exception_info:
+            elasticache_util.check_required_params(required_params, events)
+        self.assertTrue(exception_info.match('.*'))
+
+    def test_get_failover_settings(self):
+        events = {'ReplicationGroupId': REPLICATION_GROUP_ID}
+        self.mock_elasticache.describe_replication_groups.return_value = \
+            get_sample_describe_replication_groups_response(AutomaticFailover='enabled',
+                                                            MultiAZ='disabled')
+        output = elasticache_util.get_failover_settings(events, None)
+        self.mock_elasticache.describe_replication_groups.assert_called_with(
+            ReplicationGroupId=REPLICATION_GROUP_ID
+        )
+        self.assertIsNotNone(output)
+        self.assertEqual(True, output['AutomaticFailover'])
+        self.assertEqual(False, output['MultiAZ'])
+
+    def test_get_failover_settings_invalid_status(self):
+        events = {'ReplicationGroupId': REPLICATION_GROUP_ID}
+        self.mock_elasticache.describe_replication_groups.return_value = \
+            get_sample_describe_replication_groups_response(AutomaticFailover='enabling',
+                                                            MultiAZ='disabled')
+        with pytest.raises(AssertionError) as exception_info:
+            elasticache_util.get_failover_settings(events, None)
+        self.assertTrue(exception_info.match('.*'))
+
+    def test_verify_all_nodes_in_replication_group_available(self):
         events = {
-            "ReplicationGroupId": ELASTICACHE_REPLICATION_GROUP_ID
+            "ReplicationGroupId": REPLICATION_GROUP_ID
         }
         self.mock_elasticache.describe_replication_groups.return_value = \
-            get_describe_replication_groups(ELASTICACHE_REPLICATION_GROUP_ID, 3, 3)
+            get_describe_replication_groups(REPLICATION_GROUP_ID, 3, 3)
         elasticache_util.verify_all_nodes_in_rg_available(events, None)
         self.mock_elasticache.describe_replication_groups.assert_called_with(
-            ReplicationGroupId=ELASTICACHE_REPLICATION_GROUP_ID
+            ReplicationGroupId=REPLICATION_GROUP_ID
         )
 
     @patch('time.sleep')
     @patch('time.time')
-    def test_verify_all_nodes_in_rg_available_timeout(self, patched_time, patched_sleep):
+    def test_verify_all_nodes_in_replication_group_available_timeout(self, patched_time, patched_sleep):
         mock_sleep = MockSleep()
         patched_time.side_effect = mock_sleep.time
         patched_sleep.side_effect = mock_sleep.sleep
         events = {
-            "ReplicationGroupId": ELASTICACHE_REPLICATION_GROUP_ID
+            "ReplicationGroupId": REPLICATION_GROUP_ID,
+            "Sleep": 400
         }
         time_to_wait = 900
         self.mock_elasticache.describe_replication_groups.return_value = \
-            get_describe_replication_groups(ELASTICACHE_REPLICATION_GROUP_ID, 2, 3)
+            get_describe_replication_groups(REPLICATION_GROUP_ID, 2, 3)
         with pytest.raises(TimeoutError) as timeout_error:
             elasticache_util.verify_all_nodes_in_rg_available(events, None)
         self.mock_elasticache.describe_replication_groups.assert_called_with(
-            ReplicationGroupId=ELASTICACHE_REPLICATION_GROUP_ID
+            ReplicationGroupId=REPLICATION_GROUP_ID
         )
-        self.assertEqual(f'Replication group {ELASTICACHE_REPLICATION_GROUP_ID} couldn\'t '
+        self.assertEqual(f'Replication group {REPLICATION_GROUP_ID} couldn\'t '
                          f'be scaled in {time_to_wait} seconds', str(timeout_error.value))
