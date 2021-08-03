bdd4c72eea806b96d42d9748b5e304d52744c0df
iderbenev <iderbenev@upteam.com>
2021-08-03 12:31:22 +0300

    DIG-1314 [DYNDB] decrease RCU

--- documents/dynamodb/test/consume_more_rcu/2020-09-21/Tests/steps_defs/test_consume_more_rcu.py
+++ documents/dynamodb/test/consume_more_rcu/2020-09-21/Tests/steps_defs/test_consume_more_rcu.py
@@ -10,14 +10,14 @@ from pytest_bdd import (
 def test_consume_more_rcu():
     """Create AWS resources using CloudFormation template and execute SSM automation document."""
 
-#
-# @scenario('../features/consume_more_rcu_failed.feature',
-#           'Execute SSM automation document Digito-ConsumeMoreRCU_2020-09-21 to test failure case')
-# def test_consume_more_rcu_failed():
-#     """Create AWS resources using CloudFormation template and execute SSM automation document."""
-#
-#
-# @scenario('../features/consume_more_rcu_rollback_previous.feature',
-#           'Execute SSM automation document Digito-ConsumeMoreRCU_2020-09-21 in rollback')
-# def test_consume_more_rcu_rollback_previous():
-#     """Create AWS resources using CloudFormation template and execute SSM automation document."""
+
+@scenario('../features/consume_more_rcu_failed.feature',
+          'Execute SSM automation document Digito-ConsumeMoreRCU_2020-09-21 to test failure case')
+def test_consume_more_rcu_failed():
+    """Create AWS resources using CloudFormation template and execute SSM automation document."""
+
+
+@scenario('../features/consume_more_rcu_rollback_previous.feature',
+          'Execute SSM automation document Digito-ConsumeMoreRCU_2020-09-21 in rollback')
+def test_consume_more_rcu_rollback_previous():
+    """Create AWS resources using CloudFormation template and execute SSM automation document."""
