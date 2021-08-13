3f33d6c71b6ef9b9301a59e4a78f39e9be6c6820
Aydyn Tairov <atairov@upteam.com>
2021-08-13 20:10:22 +0300

    DIG-1113 Use private subnets for all LB tests

--- documents/elb/alarm/network_unhealthy_host_count/2020-04-01/Tests/step_defs/test_elb_network_unhealthy_host_count.py
+++ documents/elb/alarm/network_unhealthy_host_count/2020-04-01/Tests/step_defs/test_elb_network_unhealthy_host_count.py
@@ -11,8 +11,8 @@ def test_elb_network_unhealthy_host_count_green():
     pass
 
 
-# @scenario('../features/elb_network_unhealthy_host_count.feature',
-#           'Create elb:alarm:network_unhealthy_host_count:2020-04-01 based on UnHealthyHostCount metric '
-#           'and check ALARM status')
-# def test_elb_network_unhealthy_host_count_exceeds_threshold():
-#     pass
+@scenario('../features/elb_network_unhealthy_host_count.feature',
+          'Create elb:alarm:network_unhealthy_host_count:2020-04-01 based on UnHealthyHostCount metric '
+          'and check ALARM status')
+def test_elb_network_unhealthy_host_count_exceeds_threshold():
+    pass
