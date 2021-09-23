8c06b02343a59cfadb7034180cd5f9a5589a63d2
Dmitrij Semionov <semiond@amazon.com>
2021-09-23 13:03:48 +0300

    Skipping broken ASG alarm tests to avoid fail integration test resources.

--- documents/compute/alarm/asg-many-unhealthy-hosts/2020-07-13/Tests/step_defs/test_asg_many_unhealthy_hosts.py
+++ documents/compute/alarm/asg-many-unhealthy-hosts/2020-07-13/Tests/step_defs/test_asg_many_unhealthy_hosts.py
@@ -1,9 +1,11 @@
 # coding=utf-8
+import pytest
 from pytest_bdd import (
     scenario
 )
 
 
+@pytest.mark.skip(reason="Broken test: https://issues.amazon.com/issues/Digito-3222")
 @scenario('../features/asg_many_unhealthy_hosts.feature',
           'Lease ASG from resource manager and test attach an alarm from Document')
 def test_asg_cpu_util_alarm():
--- documents/compute/alarm/asg-multiple-unhealthy-hosts/2020-07-13/Tests/step_defs/test_asg_multiple_unhealthy_hosts.py
+++ documents/compute/alarm/asg-multiple-unhealthy-hosts/2020-07-13/Tests/step_defs/test_asg_multiple_unhealthy_hosts.py
@@ -1,9 +1,11 @@
 # coding=utf-8
+import pytest
 from pytest_bdd import (
     scenario
 )
 
 
+@pytest.mark.skip(reason="Broken test: https://issues.amazon.com/issues/Digito-3221")
 @scenario('../features/asg_multiple_unhealthy_hosts.feature',
           'Lease ASG from resource manager and test attach an alarm from Document')
 def test_asg_multiple_unhealthy_hosts_alarm():
